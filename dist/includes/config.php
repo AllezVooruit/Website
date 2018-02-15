9b894b1fc14ba102895dae3414fd44adaf45485f
Tom Beuckelaere <Tom@MacBook-Pro-van-Tom.local>
2018-02-15 16:26:18 +0100

    deleting template

--- dist/includes/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$config = new \Doctrine\DBAL\Configuration();
-$connectionParams = array(
-    'url' => 'pdo_mysql://user:secret@localhost/mydb',
-);
-$conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);

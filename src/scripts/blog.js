4a4bea6442451d00ef0a2c4354bb1a56803d76ba
Tom Beuckelaere <Tom@MacBook-Pro-van-Tom.local>
2018-03-04 15:47:45 +0100

    finishing basic css, html and JS

---
+++ src/scripts/blog.js
@@ -0,0 +1,14 @@
+;(function($) {
+
+    //Set height of blog to height of sibling
+    var setHeight = function() {
+        $('.blog__left').each(function() {
+            $(this).siblings().css('max-height', $(this).height());
+        });
+    }
+    
+    //Wait till DOM load
+    $(document).ready(function() {
+        setHeight();
+    });
+})(jQuery);

5c7c3139da6234b995d02e9d2dd57b609572d2d2
allezvooruit2018@gmail.com <diff@Joachims-MacBook-Pro.local>
2018-03-09 13:19:28 +0100

    Push Joachim

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,6 +2,14 @@
     'use strict';
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.mobile__menu__button').addEventListener('click', () => {
+        console.log(document.querySelector('.mobile__menu__button').innerHTML);
+        if(document.querySelector('.mobile__menu__button').innerHTML == '<p>MENU</p>') {
+            document.querySelector('.mobile__menu__button').innerHTML = 'SLUITEN';
+        }  
+
+        if(document.querySelector('.mobile__menu__button').innerHTML == '<p>SLUITEN</p>') {
+            document.querySelector('.mobile__menu__button').innerHTML = 'MENU';
+        }
         document.querySelector('.mobile__menu').classList.toggle('open');
     })
 });
